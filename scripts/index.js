73281392e716cb79752ff7a71e52f7693485262d
Sven Verweij <37927107+SvenVw@users.noreply.github.com>
2021-12-16 17:34:27 +0100

    Add the content code generation

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const fs  = require('fs');
+const fs = require('fs');
 
 const Lists = class {
 
@@ -6,6 +6,7 @@ const Lists = class {
         this.forms = JSON.parse(fs.readFileSync('./data/forms.json', 'utf8'));
         this.sources = JSON.parse(fs.readFileSync('./data/sources.json', 'utf8'));
         this.treatments = JSON.parse(fs.readFileSync('./data/treatments.json', 'utf8'));
+        this.content = JSON.parse(fs.readFileSync('./data/content.json', 'utf8'));
     }
 
     getFormID(form) {
@@ -20,9 +21,57 @@ const Lists = class {
         return this.treatments.find(t => t.treatment === treatment).id;
     }
 
+    getContentID(content) {
+        return this.content.find(c => c.content === content).id;
+    }
+
 }
 
-const createSagidV1 = function(source, treatment, form, nitrogen, phosphate) {
+const getFirst2digits = function (number) {
+    
+    number = number.toString().replace(/\./g, '').replace(/^0+/, '').slice(0, 2);
+
+    if (number.length === 1) {
+        number = number + '0';
+    }
+
+    return number;
+}
+
+const getExponent = function (number) {
+
+    let exponent = number.toExponential().split('e')[1];
+
+    if (exponent < 0) {
+        exponent = Math.abs(exponent)
+    } else {
+        exponent = 0;
+    }
+
+    return exponent
+}
+
+createContentCode = function (value, content) {
+
+    // Get the lists
+    const lists = new Lists();
+
+    // Lookup ID for the content
+    const contentID = lists.getContentID(content);
+
+    // Get the first 2 digits of the value
+    const contentDigits = getFirst2digits(value);
+
+    // Get the exponent
+    const contentExponent = getExponent(value);
+
+    // Create the code
+    const contentCode = `${contentID}${contentDigits}${contentExponent}`;
+
+    return contentCode;
+}
+
+const createSagidV1 = function (source, treatment, form, nitrogen, phosphorus) {
 
     // Validate the input
 
@@ -34,13 +83,23 @@ const createSagidV1 = function(source, treatment, form, nitrogen, phosphate) {
 
     // Lookup ID for treatment
     const treatmentID = lists.getTreatmentID(treatment);
-    
+
     // Lookup ID for the form
     const formID = lists.getFormID(form);
 
-    
+    // Create the code for nitrogen
+    const nitrogenCode = createContentCode(nitrogen, 'nitrogen');
+    console.log(nitrogenCode);
+
+    // Create the code for phosphorus
+    const phosphorusCode = createContentCode(phosphorus, 'phosphorus');
+    console.log(phosphorusCode);
+
     // Concate the sagid code
-    const sagid = `${sourceID}${treatmentID}${formID}`;
+    const sagid = `${sourceID}${treatmentID}${formID}${nitrogenCode}${phosphorusCode}`;
+
+    // Encode the sagid code with base58
+
 
     return sagid;
 }
--- test/test1.js
+++ test/test1.js
@@ -3,8 +3,8 @@ const sagid = require('../scripts/index');
 const source = 'cattle';
 const treatment = 'none';
 const form = 'slurry';
-const nitrogen = 27;
-const phosphate = 5;
+const nitrogen = 0.027;
+const phosphorus = 5;
 
-const sagidCode = sagid.createSagid(source, treatment, form, nitrogen, phosphate);
+const sagidCode = sagid.createSagid(source, treatment, form, nitrogen, phosphorus);
 console.log(sagidCode);
